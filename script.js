ebebd5873cd9fe5e9e48242fea4554fc9abcf102
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2023-08-31 13:45:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -74,3 +74,23 @@ headphoneToggle.addEventListener("click", () => {
     if (headphoneToggle.innerHTML.includes(`class="undeafen"`)) headphoneToggle.innerHTML = UNDEAF;
     else headphoneToggle.innerHTML = DEAF;
 });
+window.onload = function() {
+  // スクロールを禁止にする関数
+  function disableScroll(event) {
+    event.preventDefault();
+  }
+
+  // スクロール禁止
+  document.getElementById('on').onclick = function() {
+    // イベントと関数を紐付け
+    document.addEventListener('touchmove', disableScroll, { passive: false });
+    document.body.classList.add('overflow-hidden');
+  }
+
+  // スクロール解除
+  document.getElementById('off').onclick = function() {
+    // イベントと関数を紐付け
+    document.removeEventListener('touchmove', disableScroll, { passive: false });
+    document.body.classList.remove('overflow-hidden');
+  }
+}
